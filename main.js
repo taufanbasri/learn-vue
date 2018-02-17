9f2dc0c075c02c4e4f363bd6f8f81f9a1fb456e5
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-17 12:46:11 +0700

    Component within Component

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div>`,
+        
+    data() {
+        return {
+            tasks: [
+                { task: 'Learn Vue', complete: true },
+                { task: 'Learn React', complete: false },
+                { task: 'Learn Angular', complete: false },
+                { task: 'Learn Laravel', complete: true },
+            ]
+        };
+    }
+});
+
 Vue.component('task', {
-    template: '<li>Foobar</li>'
+    template: '<li><slot></slot></li>'
 });
 
 new Vue({
