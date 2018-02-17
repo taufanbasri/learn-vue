b82440df34eeba8342f26a8ad5d87b54c8a0e87e
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-17 13:57:16 +0700

    Practical Component Message Part 2

--- main.js
+++ main.js
@@ -9,18 +9,13 @@ Vue.component('message', {
         <article class="message" v-show="isVisible">
             <div class="message-header">
                 {{ title }}
-                <button @click="hideModal" class="delete" aria-label="delete"></button>
+                <button @click="isVisible = false" class="delete" aria-label="delete"></button>
             </div>
             <div class="message-body">
                 {{ body }}
             </div>
         </article>
     `,
-    methods: {
-        hideModal() {
-            this.isVisible = false
-        }
-    }
 });
 
 new Vue({
