632968933cc895dcebfd249af689f459a16ac281
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-17 14:25:42 +0700

    Practical Component Modal

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-Vue.component('message', {
-    props: ['title', 'body'],
-    data() {
-        return {
-            isVisible: true
-        }
-    },
+Vue.component('modal', {
     template: `
-        <article class="message" v-show="isVisible">
-            <div class="message-header">
-                {{ title }}
-                <button @click="isVisible = false" class="delete" aria-label="delete"></button>
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
+                </div>
             </div>
-            <div class="message-body">
-                {{ body }}
-            </div>
-        </article>
+            <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
+        </div>
     `,
 });
 
 new Vue({
-   el: '#root' 
+   el: '#root',
+   data: {
+       showModal: false
+   }
 });
