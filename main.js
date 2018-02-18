ccd6eb72c3ce11f6d537c2518353d11a7a7866c8
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-18 08:11:21 +0700

    Named Slots in a Nutshell

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-Vue.component('coupon', {
-    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
-    methods: {
-        onCouponApplied() {
-            this.$emit('applied');
-        }
-    }
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    <slot name="header">
+                    You can add default header here.
+                    </slot>
+                </p>
+                <button class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+                <slot>
+                You can add default body here.
+                </slot>
+            </section>
+            <footer class="modal-card-foot">
+                <slot name="footer">
+                    You can add default footer here.
+                </slot>
+            </footer>
+            </div>
+        </div>
+    `
 });
 
 new Vue({
    el: '#root',
-
-   data: {
-       couponApplied: false
-   },
-
-   methods: {
-       onCouponApplied() {
-           this.couponApplied = true
-       }
-   }
-});
+})
